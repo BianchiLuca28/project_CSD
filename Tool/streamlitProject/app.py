5c246d22af7bcffee92f520f24ed68814ddc5ce7
BianchiLuca28 <luca.bianchi0110@gmail.com>
2024-02-17 17:16:27 +0100

    Updating classification pie

--- Tool/streamlitProject/app.py
+++ Tool/streamlitProject/app.py
@@ -66,6 +66,30 @@ def visualize_time_series(data, selected_column):
     st.subheader(f"Time Series Visualization for {selected_column}:")
     st.plotly_chart(fig, use_container_width=True)
 
+# Function to classify time series as anomaly or not
+def classify_and_update_pie_chart(button, pie_chart, data, value_columns):
+    # Add a button to classify time series as anomaly or not
+    if button:
+        # Load the model
+        with open('../../saved_models/pruned_classifier_model.pkl', 'rb') as model_file:
+            model = pickle.load(model_file)
+            print("Model loaded successfully!")
+        
+        # Load and apply PCA to the time series
+        with open('../../saved_models/pca_transformer.pkl', 'rb') as pca_file:
+            pca = pickle.load(pca_file)
+            print("PCA loaded successfully!")
+            principalComponents = pca.transform(data[value_columns])
+            principalDf = pd.DataFrame(data = principalComponents, columns = ['principal_component_1', 'principal_component_2'])
+
+        # Predict anomalies using principal components 1 and 2
+        predictions = model.predict(principalDf)
+        
+        # Calculate the percentage of anomalies
+        percentage_anomalies = (predictions == "guasto").sum() / len(predictions) * 100
+
+        # Display pie chart
+        pie_chart.update_traces(values=[percentage_anomalies, 100 - percentage_anomalies])
 
 # Set page configuration to make the interface wider
 st.set_page_config(page_title="T-Sentry", page_icon="📈", layout="wide")
@@ -177,38 +201,21 @@ if uploaded_file is not None:
     # On the second column add the classifier and the button to classify the time series
     with col2:
         st.subheader("Classify Time Series:")
-        # Load the model
-        with open('../../saved_models/pruned_classifier_model.pkl', 'rb') as model_file:
-            model = pickle.load(model_file)
-            print("Model loaded successfully!")
-        
-        # Add a button to classify time series as anomaly or not
-        if st.button("Classify Time Series"):
-            # Load and apply PCA to the time series
-            with open('../../saved_models/pca_transformer.pkl', 'rb') as pca_file:
-                pca = pickle.load(pca_file)
-                print("PCA loaded successfully!")
-                principalComponents = pca.transform(dff[value_columns])
-                principalDf = pd.DataFrame(data = principalComponents, columns = ['principal_component_1', 'principal_component_2'])
-            
-            st.subheader("Time Series Classification:")
 
-            # Predict anomalies using principal components 1 and 2
-            predictions = model.predict(principalDf)
-            
-            # Calculate the percentage of anomalies
-            percentage_anomalies = (predictions == "guasto").sum() / len(predictions) * 100
+        # Create a button to trigger classification
+        classify_button = st.button("Classify Time Series")
 
-            # Display pie chart
-            fig_pie = go.Figure(data=[go.Pie(labels=['Anomalies', 'Normal'],
-                                             values=[percentage_anomalies, 100 - percentage_anomalies],
-                                             hole=0.3,
-                                             marker_colors=['#FF6347', '#4CAF50'])])
+        # Create an initially empty pie chart
+        pie_chart = go.Figure(go.Pie(labels=['Anomalies', 'Normal'], values=[0, 100], hole=0.3, marker_colors=['#FF6347', '#4CAF50']))
+        pie_chart.update_layout(width=300, height=300)
 
-            fig_pie.update_layout(width=300, height=300)
+        st.subheader("Time Series Classification")
 
-            # st.success('Time Series classified successfully!')
-            st.plotly_chart(fig_pie)
+        # Perform classification and update the pie chart
+        classify_and_update_pie_chart(classify_button, pie_chart, dff, value_columns)
+
+        # Display the pie chart
+        st.plotly_chart(pie_chart, use_container_width=True)
         
         st.subheader("Time Series Statistics")
         st.markdown(
